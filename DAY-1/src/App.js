23d5064e80d5a821d9eaea248b40ad017fdd87cb
abitha2005 <118422650+abitha2005@users.noreply.github.com>
2023-12-13 15:40:40 +0530

    Add files via upload

---
+++ DAY-1/src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React, { useState } from 'react';
+import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Signup/>
+      {/* <Login/> */}
+ 
+    </div>
+  );
+}
+
+export default App;
