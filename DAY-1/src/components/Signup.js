47c99e4757a9299a4b2b6e802ec63496ea75b6a0
abitha2005 <118422650+abitha2005@users.noreply.github.com>
2023-12-13 15:42:33 +0530

    Add files via upload

---
+++ DAY-1/src/components/Signup.js
@@ -0,0 +1,63 @@
+// src/App.js
+import React, { useState } from 'react';
+// import '../App.css';
+import './Signup.css'
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your sign-up logic here
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Sign up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+};
+export default Signup;
